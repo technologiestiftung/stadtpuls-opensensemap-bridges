fe5a3d90609d25127c3f33781d01608342f053a0
ff6347 <hash@ff6347.email>
2022-03-22 16:01:12 +0100

    add execution script for crontab

---
+++ to-stadtpuls/python/execute.sh
@@ -0,0 +1,1 @@
+/usr/bin/python /home/pi/stadtpuls-opensensemap-bridges/to-stadtpuls/python/main.py 2>/home/pi/log/opensensemap-to-stadtpuls.stderr.log 1>/home/pi/log/opensensemap-to-stadtpuls.stdout.log &
